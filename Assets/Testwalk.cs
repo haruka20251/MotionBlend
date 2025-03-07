080bdfd0904a34e071619558343b2eba84257786
haruka20251 <h10m3d5contrail@gmail.com>
2025-03-07 11:59:20 +0900

    Initial commit

---
+++ Assets/Testwalk.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testwalk : MonoBehaviour
+{
+    public Animator animator;
+    public float acceleration = 0.1f;//‰Á‘¬“x
+    public float deceleration = 0.5f;//Œ¸‘¬“x
+    public float maxSpeed = 1f;
+    private float speed = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed += acceleration * Time.deltaTime;
+        }
+        else
+        {
+            speed -= deceleration * Time.deltaTime * 0.1f;
+        }
+        speed = Mathf.Clamp(speed, 0f, maxSpeed);
+        animator.SetFloat("Blend", speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            {
+                animator.SetTrigger("Jump Trigger");
+            }
+        }
+    }
+}
